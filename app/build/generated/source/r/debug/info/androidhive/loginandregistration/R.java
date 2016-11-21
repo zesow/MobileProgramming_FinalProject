bb2579ed5460c25dbef8060a51369e236a534056
zesow <anstkd07@gmail.com>
2016-11-22 03:16:53 +0900

    로그인 기능. 근접 센서를 이용한 팔굽혀펴기 횟수 카운트.

--- app/build/generated/source/r/debug/info/androidhive/loginandregistration/R.java
+++ app/build/generated/source/r/debug/info/androidhive/loginandregistration/R.java
@@ -2451,15 +2451,8 @@ containing a value of this type.
         public static final int Animation_AppCompat_DropDownUp=0x7f080082;
         /**  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
          */
         public static final int AppBaseTheme=0x7f080005;
-        /**  All customizations that are NOT specific to a particular API-level can go here. 
-         */
         public static final int AppTheme=0x7f080083;
         public static final int Base_AlertDialog_AppCompat=0x7f080084;
         public static final int Base_AlertDialog_AppCompat_Light=0x7f080085;
