719aaed63b9bc654203777caba4ba8562ea05039
zesow <anstkd07@gmail.com>
2016-11-27 17:31:50 +0900

    id,score 안드로이드로 불러오기 완료. -fragment 크기를 넘을 시 fragment 크기가 커지는 현상 발생. listview를 scrollview로 변경구현해서 스크롤을 넘길수 있게 해야함. -지금 접속한 id값의 정보만 받아오게 쿼리 변경해야 함.

--- app/src/main/java/info/androidhive/loginandregistration/fragment/fragmentA.java
+++ app/src/main/java/info/androidhive/loginandregistration/fragment/fragmentA.java
@@ -3,6 +3,7 @@ package info.androidhive.loginandregistration.fragment;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,24 @@ import android.widget.Toast;
 import info.androidhive.loginandregistration.R;
 import info.androidhive.loginandregistration.activity.MainActivity;
 import info.androidhive.loginandregistration.activity.RecordActivity;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Yoo on 2016-11-27.
@@ -24,22 +43,105 @@ public class FragmentA extends Fragment {
         // Required empty public constructor
     }
 
+    String myJSON;
+
+    private static final String TAG_RESULTS="result";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADD ="count";
+
+    JSONArray peoples = null;
+
+    ArrayList<HashMap<String, String>> personList;
+
+    ListView list;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View v=inflater.inflate(R.layout.fragment_a, container, false);
 
-        v.findViewById(R.id.button3).setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View v) {
-                        Toast.makeText(getActivity(), "test", Toast.LENGTH_SHORT)
-                                .show();
-                    }
-                }
-        );
+        list = (ListView) v.findViewById(R.id.listView);
+        personList = new ArrayList<HashMap<String,String>>();
+        getData("http://54.164.7.137/android_login_api/getdata.php");
 
         return v;
+    }
+
+    protected void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for(int i=0;i<peoples.length();i++){
+                JSONObject c = peoples.getJSONObject(i);
+                String id = c.getString(TAG_ID);
+                String name = c.getString(TAG_NAME);
+                String count = c.getString(TAG_ADD);
+
+                HashMap<String,String> persons = new HashMap<String,String>();
+
+                persons.put(TAG_ID,id);
+                persons.put(TAG_NAME,name);
+                persons.put(TAG_ADD,count);
+
+                personList.add(persons);
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    getActivity(), personList, R.layout.list_item,
+                    new String[]{TAG_ID,TAG_NAME,TAG_ADD},
+                    new int[]{R.id.id, R.id.name, R.id.count}
+            );
+
+            list.setAdapter(adapter);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(String url){
+        class GetDataJSON extends AsyncTask<String, Void, String>{
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                String uri = params[0];
+
+                BufferedReader bufferedReader = null;
+                try {
+                    URL url = new URL(uri);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+
+                    bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    String json;
+                    while((json = bufferedReader.readLine())!= null){
+                        sb.append(json+"\n");
+                    }
+
+                    return sb.toString().trim();
+
+                }catch(Exception e){
+                    return null;
+                }
+
+
+
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                myJSON=result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute(url);
     }
 
 }
